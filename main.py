08558e2f1b6c66bdaa66b88060d0714101fd43e9
raul <raulelias07@hotmail.com>
2024-12-09 12:53:32 +0100

    configuramos ventana inicial y bucle principal

--- main.py
+++ main.py
@@ -15,7 +15,22 @@ pygame.display.set_caption("The Quest")
 
 reloj = pygame.time.Clock()
 
+def main():
+    ejecutando = True
+    while ejecutando:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
+    ventana.fill(NEGRO)
+
+    pygame.display.flip()
+    reloj.tick(60)
+
+
+if __name__ == "__main__":
+    main()
 
 
     
