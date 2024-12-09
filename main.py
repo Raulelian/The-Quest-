41ce65ad043df30eb7b651e458b279156fbdc6a1
raul <raulelias07@hotmail.com>
2024-12-09 12:45:01 +0100

    importamos pygame y sys y definimos variables pantalla y reloj

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+import sys
+
+pygame.init()
+
+ANCHO = 800
+ALTO= 600
+
+NEGRO = (0,0,0)
+
+ventana = pygame.display.set_mode((ANCHO,ALTO))
+
+pygame.display.set_caption("The Quest")
+
+reloj = pygame.time.Clock()
+
+
+
+
+    
