1982b1d6e6285bdc81664aad1d600c199440d07e
raul <raulelias07@hotmail.com>
2024-12-10 20:31:47 +0100

    definimos bucle principal y como detener el programa

--- juego.py
+++ juego.py
@@ -9,3 +9,26 @@ class Juego:
         self.alto= 600
         self.color_fondo= (10,10,50)
         self.ventana = pygame.display.set_mode((self.ancho, self.alto))
+        pygame.display.set_caption("The Quest")
+        self.reloj = pygame.time.Clock()
+        self.ejecutando = True
+        self.nave = Nave(self.ancho, self.alto)
+
+    def bucle_principal(self):
+        while self.ejecutando:
+            for evento in pygame.event.get():
+                if evento.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+         self.nave.mover()
+        
+         self.ventana.fill(self.color_fondo)
+         self.nave.dibujar(self.ventana)
+         
+         pygame.display.flip()
+         self.reloj.tick (60)
+        
+        
+
+
